507829206914074fe26a96dfb0dc63182e444ba8
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-25 10:12:24 +0200

    update

--- lib/widgets/navigation/navigation.dart
+++ lib/widgets/navigation/navigation.dart
@@ -11,6 +11,18 @@ class _NavigationScreenState extends State<NavigationScreen> {
   late PageController pageController;
 
   @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Container(
