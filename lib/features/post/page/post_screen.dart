db121713c06836e065d1d2e6f706b39935467b71
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-29 09:24:05 +0200

    update

--- lib/features/post/page/post_screen.dart
+++ lib/features/post/page/post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -11,9 +12,38 @@ class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [Text("l")],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text("New Post"),
+        centerTitle: false,
+        actions: [
+          Center(
+              child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
+            child: const Text(
+              "Next",
+              style: TextStyle(color: Colors.blue),
+            ),
+          ))
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 375.h,
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            mainAxisSpacing: 1,
+                            crossAxisSpacing: 1),
+                    itemBuilder: (context, index) {}),
+              )
+            ],
+          ),
         ),
       ),
     );
